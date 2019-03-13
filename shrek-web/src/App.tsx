c3cb8e0539471421aca1a4927516a100fe70ba1f
cris7ian <cristiancaroli@gmail.com>
2019-03-13 14:22:36 +0100

    Final Solution

--- shrek-web/src/App.tsx
+++ shrek-web/src/App.tsx
@@ -1,23 +1,35 @@
 import * as React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import About from "./components/about";
+import Friends from "./components/friends";
 import Home from './components/home';
+import Location from './components/location';
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-          <h1>Shrek</h1>
-          <div>
-              <div className="menu">
-                  Home | About | Location | Friends
-              </div>
-              <div className="content">
-                  <Home/>
-              </div>
-          </div>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <Router>
+                <div className="App">
+                    <h1>Shrek</h1>
+                    <div>
+                        <div className="menu">
+                            <Link to="/">Home</Link> |
+                            <Link to="/about">About</Link> |
+                            <Link to="/location">Location</Link> |
+                            <Link to="/friends">Friends</Link>
+                        </div>
+                        <div className="content">
+                            <Route path="/" exact component={Home} />
+                            <Route path="/about" component={About} />
+                            <Route path="/location" component={Location} />
+                            <Route path="/friends" component={Friends} />
+                        </div>
+                    </div>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
--- shrek-web/src/components/location.tsx
+++ shrek-web/src/components/location.tsx
@@ -3,6 +3,9 @@ import * as React from 'react';
 const Location = (): JSX.Element => (
     <div>
         <h2>The Swamp</h2>
+
+        <p>Shrek's swamp is the residence of Shrek, his wife Fiona, and their ogre triplets.</p>
+
         <img
             src="https://vignette.wikia.nocookie.net/shrek/images/5/58/Fc19d7db9cbcb7659aae3dce7bf6b3c1.jpg/revision/latest?cb=20170129025042"
             alt="the swamp"
