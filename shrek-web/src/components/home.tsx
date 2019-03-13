96e84052515b611896d269a74cf1ab1b9b16f998
cris7ian <cristiancaroli@gmail.com>
2019-03-13 16:19:51 +0100

    home wordings

--- shrek-web/src/components/home.tsx
+++ shrek-web/src/components/home.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 const Home = (): JSX.Element => (
     <div>
         <img src="http://ellwoodcity.org/wp-content/uploads/2018/02/shrek-feature.jpg" alt="good ol shrek" style={{float: 'left'}} />
-        <p>Hi, I'm Shrek (regally known as Sir Shrek) is the titular protagonist of the Shrek franchise. He is Fiona's husband, Donkey and Puss' best friend, the ogre triplets' father, and the son in law of Queen Lillian and King Harold.</p>
+        <p>Hi, I'm Shrek (regally known as Sir Shrek), the titular protagonist of the Shrek franchise. I'm Fiona's husband, Donkey and Puss' best friend, the ogre triplets' father, and the son in law of Queen Lillian and King Harold.</p>
     </div>
 );
 
